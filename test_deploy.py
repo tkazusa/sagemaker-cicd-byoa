d1ea1bb6a01089ec705d85dce5dd1be7706e4c1e
EC2 Default User <ec2-user@ip-172-16-49-163.ec2.internal>
2019-11-30 17:47:27 +0000

    mod

--- test_deploy.py
+++ test_deploy.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('--model_data', type=str)
     parser.add_argument('--role', type=str)
     parser.add_argument('--entry_point', type=str)
+    parser.add_argument('--src_dir', type=str)
     parser.add_argument('--framework_version', type=str)
     parser.add_argument('--initial_instance_count', type=str)    
     parser.add_argument('--instance_type', type=str) 
@@ -17,15 +18,16 @@ if __name__ == '__main__':
     model_data = args.model_data
     role = args.role
     entry_point = args.entry_point
+    src_dir = arg.src_dir
     framework_version = args.framework_version
     initial_instance_count = args.initial_instance_count
     instance_type = args.instance_type
-    print(role)
     
     cifar10_estimator = Model(model_data=model_data,
-                          role=role,
-                          entry_point=entry_point,
-                          framework_version=framework_version)
+                              role=role,
+                              entry_point=entry_point,
+                              source_dir=src_dir,
+                              framework_version=framework_version)
 
     cifar10_estimator.deploy(initial_instance_count=initial_instance_count,
                              instance_type=instance_type)
