3ebfcb4e15d3c7b63c5f2f0a9507e7e6f28ed5cc
EC2 Default User <ec2-user@ip-172-16-49-163.ec2.internal>
2019-11-30 06:23:48 +0000

    mod

---
+++ test_deploy.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+from sagemaker.tensorflow import TensorFlowModel
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='training script')
+    parser.add_argument('--model_path', type=str)
+    parser.add_argment('--instance_type', type=str)
+    
+
+    args = parser.parse_args()
+
+    model_data = args.model_path
+    instance_type = args.instance_type
+    
+    
+    cifar10_estimator = TensorFlowModel(model_data=model_data)
+
+    cifar10_estimator.deploy(initial_instance_count=1,
+                             instance_type=instane_type)
